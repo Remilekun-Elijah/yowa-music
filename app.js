b1439686f2c605526113c061c2d025a9c0d24106
remilekun-elijah <remilekunelijah21997@gmail.com>
2021-03-10 16:27:53 +0100

    resolving some error

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ const express = require("express"),
 app.use(morgan("dev"));
 app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, "/public"))); 
-/*app.get("/", (req, res) => {
-    res.sendFile(__dirname+/index.html))
-}; */
-app.post("/register", (req, res)=>{ 
+app.get("/", (req, res) => {
+    res.send(`<h1 style="color:teal"> Successfuly registered.</p> <p class="text-25"> Now redirecting you to the dashboard </p>`);
+    res.redirect(__dirname+"/index.html");
+});
+app.post("/", (req, res)=>{ 
     console.log(req.body);
     res.send("User has been successfully registered"); 
 });
-
+app.post("signin", (req,res)=>{
+	consoe.log(req.body);
+});
 app.listen(3000, ()=> console.log("App is listening on port 3000"));
